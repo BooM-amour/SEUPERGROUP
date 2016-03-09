4cce42df435de0f239a8355c3932e706326d52d3
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-09 10:15:41 -0800

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,26 +1,26 @@
 --------------------------------------------------
---     DEADKINGS                                --
---    DEADKINGS                                 --
---    DEADKINGS                                 --
---    DEADKINGS                                 --
---                                              --
+--     DEADKINGS                                
+--    DEADKINGS                                 
+--    DEADKINGS                                 
+--    DEADKINGS                                 
+--                                              
 --------------------------------------------------
---                                              --
---       Developer: @danyyx                     --
---     Support: @KinG0fDeaD                     --
---                                              --
---------------------------------------------------
+--                                            
+--       Developer: @danyyx                   
+--     Support: @KinG0fDeaD                   
+--                                            
+-----------------------------------------------
 
 do
 
 function run(msg, matches)
   return 'DeaD_KinGs Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nchannel[@DEAD_T34M]\n\nDevelopers :\n@danyyyx\n@KinG0fDeaD\n\nSupport :\nKinG0fDeaD' 
-  .]]
+  .
 end
 
 return {
   patterns = {
-    "^#DEADs$"
+    "^#version$"
   }, 
   run = run 
 }
